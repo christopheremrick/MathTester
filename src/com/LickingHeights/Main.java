9417e300148ae26a3ac98f0f7d5f14849742547f
Chris <christopher.emrick.2022@student.lhschools.org>
2019-02-07 13:38:56 -0500

    Created variables and used them in sys.out
    Created firstWord and secondWord

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,12 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String firstWord;
+        String secondWord;
+
+        firstWord = "dog";
+        secondWord = "house";
+
+        System.out.println(firstWord+secondWord);
     }
 }
