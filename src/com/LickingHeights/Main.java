99c5975cd01815e407a49551f8b56e0e400114ed
Chris <christopher.emrick.2022@student.lhschools.org>
2019-02-07 13:40:11 -0500

    Created two integers and assigned values
    firstNumber is 5 and secondNumber is 10

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) {
         String firstWord;
         String secondWord;
+        int firstNumber,secondNumber;
 
         firstWord = "dog";
         secondWord = "house";
+        firstNumber =  5;
+        secondNumber = 10;
 
         System.out.println(firstWord+secondWord);
     }
